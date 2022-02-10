e39f90e244a607065c5622c02564c00a6f62b942
Thomas Mahr <tpmahr@captechu.edu>
2022-02-10 14:08:54 -0500

    Renamed files for better naming convention moving forward

--- droneStatus.py
+++ droneStatus.py
@@ -12,17 +12,17 @@ numVar = 5 #The number of values we want to track
 try:
     import final
 except ModuleNotFoundError:
-    subprocess.Popen(["./droneStatus.sh", "{}".format(numVar)])
+    subprocess.Popen(["./.droneStatus.sh", "{}".format(numVar)])
     time.sleep(.5)
     import final
 
 subprocess.Popen(["node" , "navdataTest.js", "&"])
-subprocess.Popen(["./droneStatus.sh", "{}".format(numVar)])
+subprocess.Popen(["./.droneStatus.sh", "{}".format(numVar)])
 
 with output(initial_len=numVar, interval=250) as output_lines:
     while True:
         importlib.reload(final)
-        subprocess.Popen(["./droneStatus.sh", "{}".format(numVar)])
+        subprocess.Popen(["./.droneStatus.sh", "{}".format(numVar)])
         output_lines[0] = "Battery: {}".format(final.Battery)
         output_lines[1] = "Altitude: {}".format(final.Altitude)
         output_lines[2] = "Pitch: {}".format(final.Pitch)
