ae9888f2ae95820725e73bf61e9a9f45e4c296e2
Thomas Mahr <tpmahr@captechu.edu>
2022-02-14 14:58:22 -0500

    Updated header block on main script

--- droneHack.py
+++ droneHack.py
@@ -11,14 +11,12 @@ header = "\033[1;96m" + "Parrot AR Drone Attack Framework" + "\033[0m"
 authors = "\x1B[3m" + "     Developed for Capitol by Thomas Mahr and Valerie Kehrer" + "\x1B[0m"
 
 cols = int(os.popen("tput cols").read())
-rows = int(os.popen("tput lines").read())
 
 print("\n", header.rjust(int(len(header) + int(cols/3)), " "), "\n")
-print(authors.rjust(int(len(authors) + int(rows/3)) + 7, " "))
+print(authors.rjust(int(len(authors) + int(cols/4) - 8), " "))
 
-
-print("\n\n\nChecking installed software")
-os.system("./checkInstalled.sh")
+#print("\n\n\nChecking installed software")
+#os.system("./checkInstalled.sh")
 
 
 """
