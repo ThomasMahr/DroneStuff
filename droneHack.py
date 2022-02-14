487cae1d871a028eae7ce45be8efd3dee3f79c13
Thomas Mahr <tpmahr@captechu.edu>
2022-02-14 14:23:54 -0500

    Updated header block on main script

--- droneHack.py
+++ droneHack.py
@@ -7,11 +7,11 @@ import os
 
 os.system("clear")
 
-header = "\033[96m" + "Parrot AR Drone Attack Framework" + "\033[0m"
+header = "\033[1;96m" + "Parrot AR Drone Attack Framework" + "\033[0m"
 authors = "\x1B[3m" + "     Developed for Capitol by Thomas Mahr and Valerie Kehrer" + "\x1B[0m"
 
-print("\n\n\t\t\t\t" + header)
-print("\n\t\t" + authors)
+print("\n\n\t\t\t\t\t" + header)
+print("\n\t\t\t" + authors)
 
 
 print("\n\n\nChecking installed software")
