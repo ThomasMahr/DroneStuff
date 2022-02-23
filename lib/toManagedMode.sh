397938f97a810a24c11480f2e0313d62d38be87a
Thomas Mahr <tpmahr@captechu.edu>
2022-02-23 16:00:43 -0500

    Added finding drone error checking

--- lib/toManagedMode.sh
+++ lib/toManagedMode.sh
@@ -2,15 +2,21 @@
 
 MON_INT=`ifconfig | grep "wlan" | cut -d ":" -f 1`
 
+BAD='\033[1;31m'
+GOOD='\033[1;32m'
+NO_COLOR='\033[0m'
+
 if [ "$MON_INT" = "" ]
 then
-	echo "No monitor interface detected"
+	echo -e "${BAD}No monitor interface detected${NO_COLOR}"
+	echo "None" > .ints.txt
 else
-	echo "Monitor interface found: $MON_INT"
+	echo -e "Monitor interface found: ${GOOD}$MON_INT${NO_COLOR}"
 	echo "Placing into managed mode"
 	airmon-ng stop $MON_INT &> /dev/null
+	sleep 2
 	INT=`ifconfig | grep "wlan" | cut -d ":" -f 1`
-	echo "Managed interface: $INT"
+	echo -e "Managed interface: ${GOOD}$INT${NO_COLOR}"
 	echo "Man: $INT" > .ints.txt
 fi
 
--- lib/toMonitorMode.sh
+++ lib/toMonitorMode.sh
@@ -2,15 +2,20 @@
 
 INT=`ifconfig | grep "wlan" | cut -d ":" -f 1`
 
+BAD='\033[1;31m'
+GOOD='\033[1;32m'
+NO_COLOR='\033[0m'
+
 if [ "$INT" = "" ]
 then
-	echo "No wlan interface detected"
+	echo -e "${BAD}No wlan interface detected${NO_COLOR}"
+	echo "None" > .ints.txt
 else
-	echo "Wireless interface found: $INT"
+	echo -e "Wireless interface found: ${GOOD}$INT${NO_COLOR}"
 	echo "Placing into monitor mode"
 	airmon-ng start $INT &> /dev/null
 	MON_INT=`ifconfig | grep "wlan" | cut -d ":" -f 1`
-	echo "Monitor interface: $MON_INT"
+	echo -e "Monitor interface: ${GOOD}$MON_INT${NO_COLOR}"
 	echo "Mon: $MON_INT" > .ints.txt
 fi
 
