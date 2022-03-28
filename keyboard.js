7ecb0b08d86ae1dc19f908616af5fbf82b87cd1a
Valerie <vnkehrer@captechu.edu>
2022-03-28 16:38:44 -0400

    display keys and drone

--- keyboard.js
+++ keyboard.js
@@ -64,13 +64,27 @@ var client = arDrone.createClient();
 
 var readline = require('readline');
 
+//_________
+const { exec } = require("child_process");
+
+exec("cat dronePic.txt | lolcat -f ; figlet '\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tDRONEZ!!!' | lolcat -f", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`${stdout}`);
+});
+//________
 readline.emitKeypressEvents(process.stdin);
 
 
 if (process.stdin.isTTY)
         process.stdin.setRawMode(true);
-
-
+console.log("q = quit\nUp arrow = takeoff\nDown arrow = land\nLeft arrow = FTRIM (do not use during flight)\nRight arrow = magnetometer callibration (used in air)\nSpace = hover");
 process.stdin.on('keypress', (chunk, key) => {
 
         if (key && key.name == "q")
@@ -84,6 +98,12 @@ process.stdin.on('keypress', (chunk, key) => {
 	}
 	else if (key && key.name == "space"){
 		client.stop();
+	}
+	else if (key && key.name == "left"){ //FTRIM - do NOT use during flight
+		client.callibrate(1);
+	}
+	else if (key && key.name == "right"){ //magnetometer callibration - can be used during flight
+		client.callibrate(0);
 	}
 /*----------------------left hand control--------------------------*/
 	if (key && key.name == "w")
@@ -109,5 +129,7 @@ process.stdin.on('keypress', (chunk, key) => {
 	}
 	else if (key && key.name == "l"){
 		client.right(.2);
-	}	
+	}
+
+	client.stop();
 });
