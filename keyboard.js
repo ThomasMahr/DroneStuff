08548a6998360e059428e86f7e5177acd191056c
Valerie <vnkehrer@captechu.edu>
2022-03-29 14:52:07 -0400

    file fixes

--- keyboard.js
+++ keyboard.js
@@ -1,71 +1,13 @@
-//Key press basics
-//
-//
-//
-
-/*
-const readline = require('readline');
-const { Readable } = require('stream');
-
-const inStream = new Readable({
-	read() { console.log('in reading'); }
-});
-
-let i = 0;
-setInterval(() => { inStream.push(`${i++}`) }, 1000)
-readline.emitKeypressEvents(inStream);
-
-inStream.on('keypress', (...ar) => {
-	console.log(ar);
-});*/
-/*
-var readline = require('readline');
-
-readline.emitKeypressEvents(process.stdin);
-from Pyside import QtCore, QtGui;
-
-if (process.stdin.isTTY)
-	process.stdin.setRawMode(true);
-
-
-process.stdin.on('keypress', (chunk, key) => {
-	if (key && key.name == 'q')
-		process.exit();
-	else if (key) {
-		console.log("Keypressed: ", key.name);
-	}
-var pitchForward = 'i';
-var pitchBackward = 'k';
-var rollLeft = 'j';
-var rollRight
-var yawLeft
-var yawRight
-var up
-var down
-var takeoff
-var land
-
-	class KeyMapping(object){
-		PitchForward     = QtCore.Qt.Key.Key_E
-		PitchBackward    = QtCore.Qt.Key.Key_D
-		RollLeft         = QtCore.Qt.Key.Key_S
-		RollRight        = QtCore.Qt.Key.Key_F
-		YawLeft          = QtCore.Qt.Key.Key_W
-		YawRight         = QtCore.Qt.Key.Key_R
-		IncreaseAltitude = QtCore.Qt.Key.Key_Q
-		DecreaseAltitude = QtCore.Qt.Key.Key_A
-		Takeoff          = QtCore.Qt.Key.Key_Y
-		Land             = QtCore.Qt.Key.Key_H
-	}
-});*/
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
 
 
 var readline = require('readline');
 
-//_________
+
+//_________drone pic and words
 const { exec } = require("child_process");
+
 
 exec("cat /home/kali/DroneStuff/textStuff/dronePic.txt | lolcat -f ; figlet '\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tDRONEZ!!!' | lolcat -f", (error, stdout, stderr) => {
     if (error) {
@@ -78,7 +20,9 @@ exec("cat /home/kali/DroneStuff/textStuff/dronePic.txt | lolcat -f ; figlet '\t\
     }
     console.log(`${stdout}`);
 });
-//________
+//________________
+
+
 readline.emitKeypressEvents(process.stdin);
 
 
